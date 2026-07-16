e08c73f6c54f0e9cc457ac8f2c6a6e0534aa5d8f
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2026-07-16 15:07:50 -0400

    fix: local wkg config

--- wit-fetch.sh
+++ wit-fetch.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-wkg wit fetch -d crates/wasi-webgpu-wasmtime/wit/
-wkg wit fetch -d crates/frame-buffer-wasmtime/wit/
-wkg wit fetch -d crates/surface-wasmtime/wit/
-wkg wit fetch -d examples/wit/
+wkg wit fetch --config ./wkg-config.toml -d crates/wasi-webgpu-wasmtime/wit/
+wkg wit fetch --config ./wkg-config.toml -d crates/frame-buffer-wasmtime/wit/
+wkg wit fetch --config ./wkg-config.toml -d crates/surface-wasmtime/wit/
+wkg wit fetch --config ./wkg-config.toml -d examples/wit/
